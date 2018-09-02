50448281157b2721626adb119128261ec3863b33
David Hansen <david.hansen.0000@gmail.com>
2018-09-02 05:41:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function takeNumber(line, newName) {
+  line.push(newName);
+  return "Welcome, "+newName+". You are number "+(line.length+1)+" in line.";
+}
+
+function nowServing(line) {
+  var num=line.length;
+  var result;
+  if (num===0) {
+    result="There is nobody waiting to be served!";
+  } else if (num>0) {
+    var name=line.pop();
+    result = "Currently serving "+name+".";
+  }
+  return result;
+}
+
+function currentLine(line) {
+  var result;
+  if (line <= 0) {
+    result="The line is currently empty."
+  } else {
+    for (var i=0; i<line.length; i++) {
+      result = result + " " + (i+1) +". "+line[i]+", ";
+    }
+    result = result.substring(0, result.length-2);
+  }
+  return result;
+}
